508fddecbb78d0698eac97dc2d434cbb00b906b1
siddharth kathuria <kathuria.siddharth@yahoo.in>
2018-02-06 12:48:36 +0530

    moved index.html

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3000
 var app = express();
-var __dirname = '/home/sidkathuria14/Desktop/Impulse18Website';
+
 
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.static(path.join(__dirname + '/assets')));
 // app.use(express.static(__dirname));
 
 app.get('/', function(req, res, next) {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000);
