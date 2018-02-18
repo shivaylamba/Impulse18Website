8898fb829527f5e18cbc91f4c245be47f2cdd073
siddharth kathuria <kathuria.siddharth@yahoo.in>
2018-02-19 00:18:45 +0530

    final touchup

--- index.js
+++ index.js
@@ -223,6 +223,7 @@ app.post('/uiux', function (req, res, next) {
   
     if (req.body.email &&
       req.body.name &&
+      req.body.password &&
       req.body.github &&
       req.body.mobile &&
       req.body.college 
@@ -232,29 +233,43 @@ app.post('/uiux', function (req, res, next) {
       var userData = {
         email: req.body.email,
         name: req.body.name,
+        password: req.body.password,
         college:  req.body.college,
         github:   req.body.github,
-        mobile:   req.body.mobile,
+        mobile:   req.body.mobile
+        ,
         teamname: req.body.teamname
          }
-      
-           var myData = new User(req.body);
-           myData.save()
-           .then(item => {
-           res.send("item saved to database");
-           })
-           .catch(err => {
-           res.status(400).send("unable to save to database");
-           });
-      }
-       else {
-        var err = new Error('All fields required.');
-        err.status = 400;
-        return next(err);
-      }
-
-
+  
+      User.create(userData, function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          
+          req.session.userId = user._id;
+          return res.redirect('/profile');
+        }
+      });
+  
+    } else if (req.body.email && req.body.password) {
+      User.authenticate(req.body.email, req.body.password, function (error, user) {
+        if (error || !user) {
+          var err = new Error('Wrong email or password.');
+          err.status = 401;
+          return next(err);
+        } else {
+          req.session.userId = user._id;
+          return res.redirect('/profile');
+        }
+      });
+    } else {
+      var err = new Error('All fields required.');
+      err.status = 400;
+      return next(err);
+    }
   });
+
+
   
   // GET route after registering
   app.get('/profile', function (req, res, next) {
